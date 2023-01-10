91d157b0dcf9e9d24adb44d1b8f37973ecd6fb75
Jhoice Mello <78000622+jhoicemello@users.noreply.github.com>
2023-01-10 05:56:46 -0300

    Add files

---
+++ js/mobile-navbar.js
@@ -0,0 +1,62 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+
+//aparecer menu ao scrollar a página
+
+document.addEventListener('scroll', rolar);
+
+function rolar() {
+        var atualPosicao = window.scrollY;
+        var menu = document.querySelector(".menu");
+        if(atualPosicao == 0) {
+            
+            menu.classList.remove('active-color');
+        } else {
+            
+            menu.classList.add("active-color");
+        }
+
+        
+    }
